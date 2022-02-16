d08c3e05983b7eb7587b184a20aac8ead9ca9066
ankitt01 <ankitrawat0102@gmail.com>
2022-02-16 08:38:58 +0530

    Making notelist dynamic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-
+import {useState } from 'react'
+import {nanoid} from "nanoid"
 import './App.css';
 import NotesList from './components/NotesList';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note",
+      date: "15/02/22"
+  },
+  {
+    id: nanoid(),
+    text: "This is my second note",
+    date: "15/02/22"
+  },
+  {
+    id: nanoid(),
+    text: "This is my third note",
+    date: "15/02/22"
+  },
+  {
+    id: nanoid(),
+    text: "This is my fourth note",
+    date: "15/02/22"
+  }
+]);
   return (
     <div className='container' >
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Note() {
+function Note({id, text, date}) {
   return (
     <div className="note">
-      <span>Hello! This is our first note! Yayy</span>
+      <span>{text}</span>
       <div className="note-footer">
-          <small>14/02/22</small>
+          <small>{date}</small>
           <i class="fa-solid fa-trash-can"></i>
       </div>
     </div>
